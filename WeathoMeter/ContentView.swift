5d0d04cba09efc75aad1849f39aa60dd64694834
krishnaaparmar <98844700+Krishna7pk@users.noreply.github.com>
2022-08-01 16:52:54 -0400

    Initial Commit

---
+++ WeathoMeter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeathoMeter
+//
+//  Created by MobileAppDevelopment on 2022-08-01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WeathoMeter/WeathoMeterApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeathoMeterApp.swift
+//  WeathoMeter
+//
+//  Created by MobileAppDevelopment on 2022-08-01.
+//
+
+import SwiftUI
+
+@main
+struct WeathoMeterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
